b863a863f660f0e9a33fc616ede593f9864b2e1d
alansmello <alansmello@gmail.com>
2023-06-29 19:00:31 -0300

    ajuste final

--- smh/src/pages/omelete/index.js
+++ smh/src/pages/omelete/index.js
@@ -12,7 +12,7 @@ export default function Omelete() {
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <h1 className="text-4xl mb:text-6xl text-left mb-4">Maionese de Batatas</h1>
+          <h1 className="text-4xl mb:text-6xl text-left mb-4">Omelete de Legumes</h1>
 
           <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Link
